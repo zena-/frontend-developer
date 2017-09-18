acc7d8e82c1e9e5fadada9b6a3b29233cb35a5b7
npleis@gmail.com <npleis@gmail.com>
2017-09-18 11:27:41 -0600

    Setup and ready to go!

---
+++ README.md
@@ -0,0 +1,46 @@
+# Gridium Developer Application
+
+Write some code that processes the data in `./public/js/index.js` into a cool web document. Get creative! We want to see what you can do.
+
+## Instructions
+
+Fork, Code, Submit a Pull Request
+Use the libraries or framework of your choice, show us what you know!
+
+## Getting Started (if using Node/NPM)
+We're polygots. You'll find Python, R, Java, Javascript, and a few other technologies in use at Gridium. Use whatever you feel most comfortable with.
+
+If you choose to go the JS route then the basic work is done. Use NPM to initialize things.
+
+### Prerequisites
+
+Node, NPM should be installed
+
+```
+> node -v
+v7.10.0
+> npm -v
+4.2.0
+```
+
+### Installing
+
+Install project dependencies
+
+```
+> npm i
+```
+
+### Running the Project
+
+[http-server](https://www.npmjs.com/package/http-server) has been included for convenience, run with the following script:
+```
+> npm start
+Starting up http-server, serving ./public
+Available on:
+  http://127.0.0.1:8080
+  http://192.168.1.213:8080
+  http://192.168.100.1:8080
+Hit CTRL-C to stop the server
+```
+
---
+++ public/js/index.js
@@ -0,0 +1,94 @@
+var job = {
+	"headline": "Gridium Front-end Developer",
+	"essentials": {
+		"locations": "denver",
+        "employment": EmploymentType().Permanent,
+        "experience": [ExperienceLevels().Junior, ExperienceLevels().Seasoned],
+		"startdate": (new Date()).getTime(),
+		"companysize": CompanySize().TenToTwenty,
+		"teamsize": { "min": 1, "max": 6 },
+	},
+	"methodology": {
+		"codereviews": true,
+		"prototyping": true,
+		"failfast": true,
+		"unittests": true,
+		"integrationtests": true,
+		"buildserver": BuildServers().CircleCI,
+		"staticcodeanalysis": CodeAnalysisTools().NotYetChosen,
+		"versioncontrol": VersionControlSystem().Git,
+		"issuetracker": IssueTrackers().Tikkit,
+		"standups": true,
+		"quickstart": true,
+		"commitondayone": true,
+	},
+	"specs": {
+		"workload": 1.0,
+		"workweek": 40,
+        "schedule": ScheduleOptions().Flexible,
+        "remote": RemoteWorking().Required,
+        "pto": PTO().Unlimited
+	},
+	"profile": {
+		"newfeatures": 50,
+		"clientsupport": 9,
+		"documentation": 10,
+		"maintenance": 30,
+		"meetings": 1,
+	},
+	"equipment": {
+		"operatingsystem": [OperationSystems().MacOSX, OperationSystems().CentOS],
+		"computer": MachineType().Laptop,
+	},
+	"technologies": {
+        "css3": Level().Good,
+        "html5": Level().Good,
+        "javascript": Level().Good,
+        "node": Level().Good,
+        "rest": Level().Good,
+        "uiux": Level().Familiar,
+        "design": Level().Familiar,
+        "testing": {
+            "oneof": {
+                "junit": Level().Good,
+                "mocha": Level().Good,
+                "jasmine": Level().Good,
+                "selenium": Level().Good,
+            }
+        },
+        "framework": {
+            "oneof": {
+                "react": Level().Familiar,
+                "vue": Level().Familiar,
+                "angular": Level().Familiar,
+            }
+        },
+        "boardgames": Level().Familiar,
+    },
+	"other": [
+        "we love technology",
+        "we solve interesting problems"
+	]
+}
+
+function EmploymentType() { return enumerate("Permanent", "Temporary", "Project"); }
+function ExperienceLevels() { return enumerate("Junior", "Seasoned", "Lead", "GrayBeard"); }
+function CompanySize() { return enumerate("LessThanTen", "TenToTwenty", "TwentyToFifty", "FiftyToTwoHundred", "MoreThanTwoHundred"); }
+function VersionControlSystem() { return enumerate("NotYetChosen", "Git", "BitBucket"); }
+function IssueTrackers() { return enumerate("NotYetChosen", "GitHub", "Jira", "Tikkit"); }
+function BuildServers() { return enumerate("NotYetChosen", "Jenkins", "Travis", "Codeship", "CircleCI"); }
+function CodeAnalysisTools() { return enumerate("NotYetChosen", "ESLint"); }
+function KnowledgeRepos() { return enumerate("NotYetChosen", "GitHubWiki", "Confluence"); }
+function TravelOptions() { return enumerate("None", "Possible", "Plentiful"); }
+function ScheduleOptions() { return enumerate("Fixed", "Flexible"); }
+function RemoteWorking() { return enumerate("No", "Negotiable", "Required"); }
+function RelocationPackages() { return enumerate("Nonealse", "Negotiable"); }
+function OperationSystems() { return enumerate("MacOSX", "CentOS", "Ubuntu", "Windows"); }
+function MachineType() { return enumerate("Workstation", "Laptop"); }
+function Monitors() { return enumerate("Negotiable"); }
+function Level() { return enumerate("Familiar", "Good", "Expert"); }
+function TrainingType() { return enumerate("None", "Informal", "Formal", "External"); }
+function PTO() { return enumerate("Accrued", "Unlimited") }
+
+// https://github.com/RougeWare/Micro-JS-Enum/tree/master/lib
+function enumerate() { v=arguments;s={all:[],keys:v};for(i=v.length;i--;)s[v[i]]=s.all[i]=v[i];return s };
